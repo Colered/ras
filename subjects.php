b4a8fa07f8a0e4617a6a055c35f4cbf746e23296
unknown <kalicharan.sikarwar@IBGGN8858-L.ibtechnology.com>
2014-11-06 15:30:53 +0530

    one file remains to do alphanumeric validation

--- subjects.php
+++ subjects.php
@@ -135,7 +135,7 @@ $progId = isset($_GET['edit']) ? $row['program_year_id'] : (isset($_POST['slctPr
 						<div class="txtfield ">
 						<div class="sessionboxSub" style="width:110px;">
 						<h3>Session Name<span class="redstar">*</span></h3>
-							<input type="text" class="inp_txt_session alphanumeric" <?php echo $disSession; ?> id="txtSessionName" maxlength="50" style="width:94px;" name="txtSessionName" value="">
+							<input type="text" class="inp_txt_session required" <?php echo $disSession; ?> id="txtSessionName" maxlength="50" style="width:94px;" name="txtSessionName" value="">
 						</div>
 						<!--<div class="sessionboxSub" style="width:110px;">
 						<h3>Order Number<span class="redstar">*</span></h3>
