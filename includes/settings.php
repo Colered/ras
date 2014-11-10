ffcceb7ceffa3e46f91924f4a55b2e43e2e735ba
kakkard <deepali.kakkar@ibtechnology.com>
2014-11-10 12:51:24 +0530

    changes in calendar settings

--- includes/settings.php
+++ includes/settings.php
@@ -2,10 +2,10 @@
 /* updated via install/index.php on Thu, 21 Aug 2014 05:36:13 -0400 
 install_password: 0192023a7bbd73250516f069df18b500
 db_type: mysql
-db_host: localhost
+db_host: 172.16.220.164
 db_database: cidot_ras
 db_login: root
-db_password: none
+db_password: cidot@123
 db_persistent: false
 readonly: false
 user_inc: user.php
