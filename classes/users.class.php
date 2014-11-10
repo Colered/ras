30e2f34303693a787357ba3fc546feae1c840ad0
unknown <kalicharan.sikarwar@IBGGN8858-L.ibtechnology.com>
2014-11-10 11:27:04 +0530

    changes for the character encoding

--- classes/users.class.php
+++ classes/users.class.php
@@ -15,7 +15,7 @@ class Users extends Base {
 			{
 				while($data = mysqli_fetch_assoc($q_res))
 	  			{
-					$_SESSION['user_id']=$data['id']; 
+					$_SESSION['user_id']=$data['id'];
 					$_SESSION['username']=$data['username'];
 					$_SESSION['user_email']=$data['email'];
 					$_SESSION['role_id']=$data['role_id'];
@@ -66,7 +66,7 @@ class Users extends Base {
 							$message= "Cannot send password to your e-mail address.Problem with sending mail...";
 							$_SESSION['error_msg'] = $message;
 							return 0;
-						}	
+						}
 					 }
 				} else {
 					if ($_POST ['email'] != "") {
@@ -83,7 +83,7 @@ class Users extends Base {
 		$query = mysqli_query($this->conn, $sql);
 		while ($row = mysqli_fetch_array($query)) {
 			$username = $row['username'];
-			$password = $row['password']; 
+			$password = $row['password'];
 		}
 		$cur_password=base64_encode($_POST['currentPassword']);
 		$new_pwd=base64_encode($_POST['newPassword']);
