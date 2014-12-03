a4020c39a73877f90b439a0f7a3a8b38781b08a8
unknown <singh.ravendra@IBGGN8857-L.grgh.indiabulls.com>
2014-12-03 18:22:38 +0530

    fix for additional day bug

--- ajax_common.php
+++ ajax_common.php
@@ -1175,15 +1175,14 @@ switch ($codeBlock) {
                                 }
                             }
                         }
-						
 						//check for additional days availability
-						$sql_pgm_add_date = $this->conn->query("select additional_date,actual_timeslot_id from program_cycle_additional_day_time where additional_date between  '".$result_pgm_cycle['start_week']."' and '".$result_pgm_cycle['end_week']."' and program_year_id = '".$result_pgm_cycle['program_year_id']."'");
+						$sql_pgm_add_date = mysqli_query($db, "select additional_date,actual_timeslot_id from program_cycle_additional_day_time where additional_date between  '".$result_pgm_cycle['start_week']."' and '".$result_pgm_cycle['end_week']."' and program_year_id = '".$result_pgm_cycle['program_year_id']."'");
 						while($result_pgm_add_date = mysqli_fetch_array($sql_pgm_add_date))
 						{
 							$ts_array = explode(",",$result_pgm_add_date['actual_timeslot_id']);
-							if(array_key_exists($result_pgm_add_date['additional_date'],$final_programs[$result_pgm_cycle['program_year_id']][$result_pgm_cycle['id']]))
+							if(array_key_exists($result_pgm_add_date['additional_date'],$final_programs))
 							{
-								$new_arr = array_unique(array_merge($final_programs[$result_pgm_cycle['program_year_id']][$result_pgm_cycle['id']][$result_pgm_add_date['additional_date']],$ts_array));		
+								$new_arr = array_unique(array_merge($final_programs,$ts_array));		
 								$final_programs[$result_pgm_cycle['program_year_id']][$result_pgm_cycle['id']][$result_pgm_add_date['additional_date']] = $new_arr;
 							}else{
 								$final_programs[$result_pgm_cycle['program_year_id']][$result_pgm_cycle['id']][$result_pgm_add_date['additional_date']] = $ts_array;
